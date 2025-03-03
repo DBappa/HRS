da53b8963883b92073c5043d96aae5984b6e9d23
Uttam154 <uttamghosh154@gmail.com>
2025-03-03 15:41:18 +0530

    add Navbar,Sidebar and Footer

--- Hotel-Reservation-System/resources/views/admin/admin_dashboard.blade.php
+++ Hotel-Reservation-System/resources/views/admin/admin_dashboard.blade.php
@@ -1,8 +1,12 @@
-<html>
-    <title>
-        Admin Dashboard
-    </title>
-    <body>
-        <h1>Admin Dashboard</h1>
-    </body>
-</html>
+@extends('layouts.admin')
+
+@section('content')
+    <h2>Welcome to Admin Dashboard</h2>
+    <p>Manage reservations, rooms, and users here.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem error repellendus commodi ipsam iure quis dolores alias, voluptates laborum natus sequi magni quidem vel in reprehenderit accusamus eos expedita placeat?
+    Dicta veniam dolor, facilis maiores sunt, molestias, atque quam delectus ipsa quis autem obcaecati alias tempore eum tenetur totam repellendus iusto odit ullam. Esse veniam officia consequuntur ipsum harum sequi?
+    Earum veniam, rerum voluptatum error ipsa optio quibusdam quod qui. Laboriosam minus ut dolorem eum? Non, voluptas odit cum numquam optio officia, minima eveniet quia quaerat beatae, vitae enim deleniti.
+    Vel asperiores repellendus fugit impedit pariatur, accusantium molestiae quis amet voluptatibus cum id? Ad voluptas ratione explicabo ea. Quibusdam vero amet, atque ab doloribus eligendi vel consequatur facere voluptates unde!
+    Mollitia dolore ipsa vitae, maxime quos minus tempore labore doloremque rem? Corporis magnam distinctio consequuntur nostrum repudiandae, veritatis minima eos perspiciatis atque laborum excepturi accusantium, cupiditate a unde deserunt nobis!
+    Itaque, at. Nulla optio, neque voluptate aut dolores nesciunt officia sed impedit ad aspernatur quam suscipit eum eveniet autem nisi ipsum voluptates accusantium aliquam excepturi obcaecati vero error temporibus quo?</p>
+@endsection
--- Hotel-Reservation-System/routes/web.php
+++ Hotel-Reservation-System/routes/web.php
@@ -23,5 +23,12 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');  
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+// Route::middleware(['auth', 'admin'])->group(function (){
+
+// Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard'); 
+// Route::get('/admin/reservations', [AdminController::class, 'reservations'])->name('admin.reservations');
+// Route::get('/admin/rooms', [AdminController::class, 'rooms'])->name('admin.rooms');
+// });
